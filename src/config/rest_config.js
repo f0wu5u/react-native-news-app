ea2f2f2b418b9106ef9bd533357e9075a02e6a55
Frank Owusu <codewithfrank@gmail.com>
2018-06-28 03:59:00 +0000

    Update rest_config.js

--- src/config/rest_config.js
+++ src/config/rest_config.js
@@ -2,4 +2,4 @@ export const  articles_url = "https://newsapi.org/v2/top-headlines/";
 export const language = "en";
 export const category = "general";
 
-export const _api_key = "ea3aefaaa08a4897a72a6e2ffeab9c52";
+export const _api_key = "YOUR_API_KEY";
